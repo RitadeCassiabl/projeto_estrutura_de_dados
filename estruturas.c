2e4d9f120b546f8e7d2e9dd24c55d9e6b462d6bc
paulohenriquedev <paulohenrique.root@gmail.com>
2026-06-02 17:59:10 -0300

    Ajuste para fila duplamente encadeada

--- estruturas.c
+++ estruturas.c
@@ -4,67 +4,89 @@
 
 #include "headers/estruturas.h"
 
-void inicializar_fila(Fila* f) {
+void inicializar_fila(Fila *f)
+{
     f->frente = NULL;
     f->tras = NULL;
     f->tamanho = 0;
 }
 
-int fila_vazia(Fila* f) {
+int fila_vazia(Fila *f)
+{
     return f->frente == NULL;
 }
 
-void enfileirar(Fila* f, const char* usuario) {
-    NoFila* novo = (NoFila*)malloc(sizeof(NoFila));
+void enfileirar(Fila *f, const char *usuario)
+{
+    NoFila *novo = (NoFila *)malloc(sizeof(NoFila));
     strcpy(novo->nome_usuario, usuario);
     novo->proximo = NULL;
+    novo->anterior = NULL;
 
-    if (fila_vazia(f)) {
+    if (fila_vazia(f))
+    {
         f->frente = novo;
-    } else {
-        f->tras->proximo = novo;
     }
+    else
+    {
+        f->tras->proximo = novo;
+        novo->anterior = f->tras;
+    }
+
     f->tras = novo;
     f->tamanho++;
 }
 
-int desenfileirar(Fila* f, char* destino_nome) {
-    if (fila_vazia(f)) return 0;
+int desenfileirar(Fila *f, char *destino_nome)
+{
+    if (fila_vazia(f))
+        return 0;
 
-    NoFila* temp = f->frente;
+    NoFila *temp = f->frente;
     strcpy(destino_nome, temp->nome_usuario);
-    
+
     f->frente = f->frente->proximo;
-    if (f->frente == NULL) {
+
+    if (f->frente == NULL)
+    {
         f->tras = NULL;
     }
-    
+    else
+    {
+        f->frente->anterior = NULL;
+    }
+
     free(temp);
     f->tamanho--;
     return 1;
 }
 
-void inicializar_pilha(Pilha* p) {
+void inicializar_pilha(Pilha *p)
+{
     p->topo = NULL;
 }
 
-int pilha_vazia(Pilha* p) {
+int pilha_vazia(Pilha *p)
+{
     return p->topo == NULL;
 }
 
-void empilhar(Pilha* p, const char* titulo) {
-    NoPilha* novo = (NoPilha*)malloc(sizeof(NoPilha));
+void empilhar(Pilha *p, const char *titulo)
+{
+    NoPilha *novo = (NoPilha *)malloc(sizeof(NoPilha));
     strcpy(novo->titulo_livro, titulo);
     novo->proximo = p->topo;
     p->topo = novo;
 }
 
-int desempilhar(Pilha* p, char* destino_titulo) {
-    if (pilha_vazia(p)) return 0;
+int desempilhar(Pilha *p, char *destino_titulo)
+{
+    if (pilha_vazia(p))
+        return 0;
 
-    NoPilha* temp = p->topo;
+    NoPilha *temp = p->topo;
     strcpy(destino_titulo, temp->titulo_livro);
-    
+
     p->topo = p->topo->proximo;
     free(temp);
     return 1;
--- headers/estruturas.h
+++ headers/estruturas.h
@@ -3,34 +3,39 @@
 
 #define MAX_STR 50
 
-typedef struct NoFila {
+typedef struct NoFila
+{
     char nome_usuario[MAX_STR];
-    struct NoFila* proximo;
+    struct NoFila *proximo;
+    struct NoFila *anterior;
 } NoFila;
 
-typedef struct {
-    NoFila* frente;
-    NoFila* tras;
+typedef struct
+{
+    NoFila *frente;
+    NoFila *tras;
     int tamanho;
 } Fila;
 
-void inicializar_fila(Fila* f);
-int fila_vazia(Fila* f);
-void enfileirar(Fila* f, const char* usuario);
-int desenfileirar(Fila* f, char* destino_nome);
+void inicializar_fila(Fila *f);
+int fila_vazia(Fila *f);
+void enfileirar(Fila *f, const char *usuario);
+int desenfileirar(Fila *f, char *destino_nome);
 
-typedef struct NoPilha {
+typedef struct NoPilha
+{
     char titulo_livro[MAX_STR];
-    struct NoPilha* proximo;
+    struct NoPilha *proximo;
 } NoPilha;
 
-typedef struct {
-    NoPilha* topo;
+typedef struct
+{
+    NoPilha *topo;
 } Pilha;
 
-void inicializar_pilha(Pilha* p);
-int pilha_vazia(Pilha* p);
-void empilhar(Pilha* p, const char* titulo);
-int desempilhar(Pilha* p, char* destino_titulo);
+void inicializar_pilha(Pilha *p);
+int pilha_vazia(Pilha *p);
+void empilhar(Pilha *p, const char *titulo);
+int desempilhar(Pilha *p, char *destino_titulo);
 
 #endif
