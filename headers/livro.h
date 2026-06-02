5e8546b583cd92cfb91d42c6007f3c2528f38680
Rita <rita.jxlm.27.7.f@gmail.com>
2026-06-02 00:46:13 -0300

    Co-authored-by: PauloHenrique <paulohenrique.root@gmail.com> Co-authored-by: jvsg5678@outlook.com <jvsg5678@outlook.com> Co-authored-by: Jotinha <jotave5678@users.noreply.github.com>

--- headers/livro.h
+++
@@ -1,9 +0,0 @@
-typedef struct livro Livro;
-
-Livro* criarLivro(Livro* inicio, char* titulo, char* genero, char* editora, char* autor, int ano);
-
-Livro* criarListaLivros();
-
-void imprimirLivros(Livro *lista);
-
-void excluirlivros(Livro *lista);
